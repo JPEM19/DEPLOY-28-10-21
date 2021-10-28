4cf3a580d702b0250dde6dd1cbd693cf2427287a
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-28 16:24:11 -0500

    Update
    redefinition of method

--- palindrome/test_palindrome.py
+++ palindrome/test_palindrome.py
@@ -5,14 +5,14 @@ class TestPalindrome:
     def test_palindrome(self):
         assert "palindrome" == palindrome.check("ana") 
 
-    def test_palindrome(self):
+    def test_palindrome2(self):
         assert "not palindrome" == palindrome.check("abc")  
 
-    def test_palindrome(self):
+    def test_palindrome3(self):
         assert "palindrome" == palindrome.check("nadan")
 
-    def test_palindrome(self):
+    def test_palindrome4(self):
         assert "palindrome" == palindrome.check("reconocer")     
 
-    def test_palindrome(self):
+    def test_palindrome5(self):
         assert "not palindrome" == palindrome.check("elefante") 
