409fa2eb8a881ec1cc808875074a2f3cca30d45a
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-28 16:19:41 -0500

    Update tes_palindrome.py
    Format changes

--- palindrome/test_palindrome.py
+++ palindrome/test_palindrome.py
@@ -1,18 +1,18 @@
 import palindrome
 
-class TestPalindrome:
 
- def test_palindrome(self):
-    assert "palindrome" == palindrome.check("ana") 
+class TestPalindrome:
+    def test_palindrome(self):
+        assert "palindrome" == palindrome.check("ana") 
     
- def test_palindrome(self):
-    assert "not palindrome" == palindrome.check("abc")  
+    def test_palindrome(self):
+        assert "not palindrome" == palindrome.check("abc")  
     
- def test_palindrome(self):
-    assert "palindrome" == palindrome.check("nadan")
+    def test_palindrome(self):
+        assert "palindrome" == palindrome.check("nadan")
     
- def test_palindrome(self):
-    assert "palindrome" == palindrome.check("reconocer")     
+    def test_palindrome(self):
+        assert "palindrome" == palindrome.check("reconocer")     
     
- def test_palindrome(self):
-    assert "not palindrome" == palindrome.check("elefante") 
+    def test_palindrome(self):
+        assert "not palindrome" == palindrome.check("elefante") 
