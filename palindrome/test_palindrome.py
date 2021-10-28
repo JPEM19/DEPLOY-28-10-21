85d4f0baef427ee2225986523c8ca7ceebfdd848
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-28 16:22:01 -0500

    update test_palindrome

--- palindrome/test_palindrome.py
+++ palindrome/test_palindrome.py
@@ -4,15 +4,15 @@ import palindrome
 class TestPalindrome:
     def test_palindrome(self):
         assert "palindrome" == palindrome.check("ana") 
-    
+
     def test_palindrome(self):
         assert "not palindrome" == palindrome.check("abc")  
-    
+
     def test_palindrome(self):
         assert "palindrome" == palindrome.check("nadan")
-    
+
     def test_palindrome(self):
         assert "palindrome" == palindrome.check("reconocer")     
-    
+
     def test_palindrome(self):
         assert "not palindrome" == palindrome.check("elefante") 
