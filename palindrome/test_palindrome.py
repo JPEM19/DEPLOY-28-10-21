077ab27c65f11720de1c88d0b7a5bbafe666a0bf
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-28 16:26:12 -0500

    Final changes

--- palindrome/test_palindrome.py
+++ palindrome/test_palindrome.py
@@ -3,16 +3,16 @@ import palindrome
 
 class TestPalindrome:
     def test_palindrome(self):
-        assert "palindrome" == palindrome.check("ana") 
+        assert "palindrome" == palindrome.check("ana")
 
     def test_palindrome2(self):
-        assert "not palindrome" == palindrome.check("abc")  
+        assert "not palindrome" == palindrome.check("abc")
 
     def test_palindrome3(self):
         assert "palindrome" == palindrome.check("nadan")
 
     def test_palindrome4(self):
-        assert "palindrome" == palindrome.check("reconocer")     
+        assert "palindrome" == palindrome.check("reconocer")
 
     def test_palindrome5(self):
-        assert "not palindrome" == palindrome.check("elefante") 
+        assert "not palindrome" == palindrome.check("elefante")
