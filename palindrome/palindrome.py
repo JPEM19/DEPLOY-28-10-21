61ee944eeb2de55798e41f95b92f88ba06a73afe
Juan Pablo Escobedo Muñoz <61511918+JPEM19@users.noreply.github.com>
2021-10-28 16:14:44 -0500

    Update config.yml
    Final updates

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,8 +1,10 @@
 """
 String to know if it is a palindrome
 """
+
+
 def check(word):
-    if(word==word[::-1]):
+    if (word==word[::-1]):
         return "palindrome"
     else:
         return "not palindrome"
